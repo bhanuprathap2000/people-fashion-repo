233cec5d16d157d551835870d5a194ea6050ec5f
bhanuprathapk2000 <bhanu.prathap@shiryam.com>
2021-09-04 19:14:00 +0530

    added the registration and login with email and password

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,14 +1,58 @@
 import React, { Component } from 'react';
 import './styles.scss';
-import Buttons from '../forms/Buttons';
-import { signInWithGoogle } from './../../firebase/utils';
+import Button from './../forms/Buttons';
+import { signInWithGoogle, auth } from './../../firebase/utils';
+
+import FormInput from './../forms/FormInput';
+
+const initialState = {
+	email: '',
+	password: '',
+};
 
 class SignIn extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			...initialState,
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(e) {
+		//this function responsibele for updating the state
+		const { name, value } = e.target;
+		this.setState({
+			[name]: value,
+		});
+	}
+
 	handleSubmit = async (e) => {
+		/*
+		
+		so this function will run when we click on the submit and we destruture the email and password
+		and then using  the auth instance call the siginWithemailandpassword and pass the details
+		we are doing this in try catch block because their might be any error that can occur during the
+		sigin
+		when it is sucessful we reset the form 
+		*/
 		e.preventDefault();
+		const { email, password } = this.state;
+
+		try {
+			await auth.signInWithEmailAndPassword(email, password);
+			this.setState({
+				...initialState,
+			});
+		} catch (err) {
+			// console.log(err);
+		}
 	};
 
 	render() {
+		const { email, password } = this.state;
+
 		return (
 			<div className="signin">
 				<div className="wrap">
@@ -16,11 +60,29 @@ class SignIn extends Component {
 
 					<div className="formWrap">
 						<form onSubmit={this.handleSubmit}>
+							<FormInput
+								type="email"
+								name="email"
+								value={email}
+								placeholder="Email"
+								handleChange={this.handleChange}
+							/>
+
+							<FormInput
+								type="password"
+								name="password"
+								value={password}
+								placeholder="Password"
+								handleChange={this.handleChange}
+							/>
+
+							<Button type="submit">LogIn</Button>
+
 							<div className="socialSignin">
 								<div className="row">
-									<Buttons onClick={signInWithGoogle}>
+									<Button onClick={signInWithGoogle}>
 										Sign in with Google
-									</Buttons>
+									</Button>
 								</div>
 							</div>
 						</form>
---
+++ src/components/Signup/index.js
@@ -0,0 +1,137 @@
+import React, { Component } from 'react';
+import './styles.scss';
+
+import { auth, handleUserProfile } from './../../firebase/utils';
+
+import FormInput from './../forms/FormInput';
+import Button from './../forms/Button';
+
+const initialState = {
+	displayName: '',
+	email: '',
+	password: '',
+	confirmPassword: '',
+	errors: [],
+};
+
+class Signup extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			...initialState,
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(e) {
+		const { name, value } = e.target;
+
+		this.setState({
+			[name]: value,
+		});
+	}
+
+	handleFormSubmit = async (event) => {
+		/*
+      this function will run when we try to register 
+      get the details from the form and check whether the password and confirm password are the same
+      if not render those errors by setting the error state
+      if no errors then call the auth.createUserWithEmailAndPassword with the details
+      and once the user is created we get back the user
+      but not over this just creteas the record for firebase but we are maintaining the
+      user in seperate collection hence we need to pass the user reference to the
+      handleUserProfile and then if exisits reference is created of not then new user is created 
+      and then reset the form 
+      we are doing this in try catch form because if we try register with existing email
+      then firebase maynot allow that's to catch the error using the try catch
+      
+      */
+		event.preventDefault();
+		const { displayName, email, password, confirmPassword } = this.state;
+
+		if (password !== confirmPassword) {
+			const err = ["Password Don't match"];
+			this.setState({
+				errors: err,
+			});
+			return;
+		}
+
+		try {
+			const { user } = await auth.createUserWithEmailAndPassword(
+				email,
+				password
+			);
+
+			await handleUserProfile(user, { displayName });
+
+			this.setState({
+				...initialState,
+			});
+		} catch (err) {
+			// console.log(err);
+		}
+	};
+
+	render() {
+		const { displayName, email, password, confirmPassword, errors } =
+			this.state;
+
+		return (
+			<div className="signup">
+				<div className="wrap">
+					<h2>Signup</h2>
+
+					{errors.length > 0 && (
+						<ul>
+							{errors.map((err, index) => {
+								return <li key={index}>{err}</li>;
+							})}
+						</ul>
+					)}
+
+					<div className="formWrap">
+						<form onSubmit={this.handleFormSubmit}>
+							<FormInput
+								type="text"
+								name="displayName"
+								value={displayName}
+								placeholder="Full name"
+								onChange={this.handleChange}
+							/>
+
+							<FormInput
+								type="email"
+								name="email"
+								value={email}
+								placeholder="Email"
+								onChange={this.handleChange}
+							/>
+
+							<FormInput
+								type="password"
+								name="password"
+								value={password}
+								placeholder="Password"
+								onChange={this.handleChange}
+							/>
+
+							<FormInput
+								type="password"
+								name="confirmPassword"
+								value={confirmPassword}
+								placeholder="Confirm Password"
+								onChange={this.handleChange}
+							/>
+
+							<Button type="submit">Register</Button>
+						</form>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Signup;
