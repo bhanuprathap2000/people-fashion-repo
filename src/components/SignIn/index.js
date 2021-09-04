3bf67b8b33d08f72cff91cb46804d7147445c8f5
bhanuprathapk2000 <bhanu.prathap@shiryam.com>
2021-09-04 23:31:05 +0530

    implemented the reset password feature

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import './styles.scss';
 import Button from './../forms/Buttons';
 import { signInWithGoogle, auth } from './../../firebase/utils';
-
 import FormInput from './../forms/FormInput';
+import AuthWrapper from '../AuthWrapper';
+import { Link } from 'react-router-dom';
 
 const initialState = {
 	email: '',
@@ -47,7 +48,7 @@ class SignIn extends Component {
 				...initialState,
 			});
 		} catch (err) {
-			console.log(err);
+			console.table(err);
 			this.setState({
 				errors: [err.message],
 			});
@@ -55,52 +56,52 @@ class SignIn extends Component {
 	};
 
 	render() {
-		const { email, password,errors } = this.state;
+		const { email, password, errors } = this.state;
+		const configAuthWrapper = {
+			headline: 'LogIn',
+		};
 
 		return (
-			<div className="signin">
-				<div className="wrap">
-					<h2>LogIn</h2>
-					{errors.length > 0 && (
-						<ul>
-							{errors.map((err, index) => {
-								return <li key={index}>{err}</li>;
-							})}
-						</ul>
-					)}
+			<AuthWrapper {...configAuthWrapper}>
+				{errors.length > 0 && (
+					<ul>
+						{errors.map((err, index) => {
+							return <li key={index}>{err}</li>;
+						})}
+					</ul>
+				)}
 
+				<div className="formWrap">
+					<form onSubmit={this.handleSubmit}>
+						<FormInput
+							type="email"
+							name="email"
+							value={email}
+							placeholder="Email"
+							handleChange={this.handleChange}
+						/>
 
-					<div className="formWrap">
-						<form onSubmit={this.handleSubmit}>
-							<FormInput
-								type="email"
-								name="email"
-								value={email}
-								placeholder="Email"
-								handleChange={this.handleChange}
-							/>
+						<FormInput
+							type="password"
+							name="password"
+							value={password}
+							placeholder="Password"
+							handleChange={this.handleChange}
+						/>
 
-							<FormInput
-								type="password"
-								name="password"
-								value={password}
-								placeholder="Password"
-								handleChange={this.handleChange}
-							/>
+						<Button type="submit">LogIn</Button>
 
-							<Button type="submit">LogIn</Button>
-
-							<div className="socialSignin">
-								<div className="row">
-									<Button onClick={signInWithGoogle}>
-										Sign in with Google
-									</Button>
-								</div>
+						<div className="socialSignin">
+							<div className="row">
+								<Button onClick={signInWithGoogle}>Sign in with Google</Button>
 							</div>
-						</form>
-					</div>
+						</div>
+						<div className="links">
+							<Link to="/recovery">Reset Password</Link>
+						</div>
+					</form>
 				</div>
-			</div>
+			</AuthWrapper>
 		);
 	}
 }
